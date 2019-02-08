023d12e2ed6a9738e49c6b6d32d1f9b6ab8779b1
Zhou Fang <zhoufang@ucsd.edu>
2019-02-07 22:10:24 -0800

    add demo file

---
+++ codes/data/dstl_dataset/test_dataset.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import torch
+import torch.utils.data as data
+
+from data.dstl_dataset.image_data import ImageData
+from data.dstl_dataset.preprocess_utils import adjust_size, downsample, rand_rotate_and_crop
+
+
+class DstlDataset(data.Dataset):
+
+    def __init__(self, opt):
+        super(DstlDataset, self).__init__()
+        # data_dir = '/Users/zhoufang/Desktop/lu/dstl'
+        data_dir = opt['dataroot_HR']
+        train_wkt_v4 = pd.read_csv(os.path.join(data_dir, 'train_wkt_v4.csv'))
+        grid_sizes = pd.read_csv(os.path.join(data_dir, 'grid_sizes.csv'),
+                    skiprows=1, names=['ImageId', 'Xmax', 'Ymin'])
+
+        train_names = sorted(train_wkt_v4.ImageId.unique())
+
+        x_crop = 3345
+        y_crop = 3338
+        self.scale = opt['scale']
+        self.patch_size = opt['HR_size']
+        self.images = []
+        self.labels = []
+
+        for img_id in train_names[0:1]:
+            image_data = ImageData(data_dir, img_id, grid_sizes, train_wkt_v4)
+            image_data.create_train_feature()
+            image_data.create_label()
+
+            #cv2.imshow('image', (image_data.train_feature[1000:2000, 1000:2000]/3.0).astype(np.uint8))
+            #cv2.waitKey(0)
+            #plt.imshow((image_data.train_feature/3).astype(np.uint8))
+            #plt.show()
+            #break
+
+            # Reduce the dimension of label classes.
+            image_data.label[:, :, 6] = image_data.label[:, :, 6] + image_data.label[:, :, 7]
+            image_data.label[:, :, 7] = image_data.label[:, :, 8] + image_data.label[:, :, 9] 
+
+            self.images.append(image_data.train_feature[:x_crop, :y_crop, :])
+            self.labels.append(image_data.label[:x_crop, :y_crop, :8])
+
+        self.total_imgs = len(self.images)
+
+
+    def __getitem__(self, index):
+        rand_idx = np.random.randint(self.total_imgs)
+        image = self.images[rand_idx]
+        label = self.labels[rand_idx]
+
+        image = adjust_size(image, self.scale)
+        label = adjust_size(label, self.scale)
+        image, label = rand_rotate_and_crop(image, label, self.patch_size)
+        # TODO(coufon): scale image to [0, 1].
+        image = image/2000.0
+        image_lr = downsample(image, self.scale)
+
+        return {
+            'LR': torch.from_numpy(np.ascontiguousarray(
+                np.transpose(image_lr.astype(np.float), (2, 0, 1))))[np.new_axis, :, :, :].float(),
+            'HR': torch.from_numpy(np.ascontiguousarray(
+                np.transpose(image.astype(np.float), (2, 0, 1)))).float(),
+            'seg': torch.from_numpy(np.ascontiguousarray(
+                np.transpose(label.astype(np.float), (2, 0, 1))))[np.new_axis, :, :, :].float()
+        }
+
+
+    def __len__(self):
+        return self.total_imgs * 100
---
+++ codes/demo.py
@@ -0,0 +1,47 @@
+import argparse
+import matplotlib.pyplot as plt
+import os.path
+import glob
+import cv2
+import numpy as np
+import torch
+
+from data.util import imresize, modcrop
+import utils.util as util
+import models.modules.sft_arch as sft
+from data.dstl_dataset.test_dataset import DstlDataset
+import options.options as option
+
+
+# model_path = '../experiments/pretrained_models/sft_net_torch.pth' # torch version
+model_path = '../models/206000_G.pth'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-opt', type=str, required=True, help='Path to option JSON file.')
+opt = option.parse(parser.parse_args().opt, is_train=True)
+opt = option.dict_to_nonedict(opt)  # Convert to NoneDict, which return None for missing key.
+
+train_set = DstlDataset(opt['datasets']['train'])
+
+if 'torch' in model_path:  # torch version
+    model = sft.SFT_Net_torch()
+else:
+    model = sft.SFT_Net()
+model.load_state_dict(torch.load(model_path), strict=True)
+model.eval()
+model = model.cuda()
+
+print('sftgan testing...')
+
+for i in range(100):
+    data = train_set[i]
+    img_HR = data['HR']
+    img_LR = data['LR']
+    seg = data['seg']
+    output = model((img_LR, seg)).data
+    
+    util.save_img(util.tensor2img(img_HR.squeeze()),
+        os.path.join('../results', 'hr.png'))
+
+    util.save_img(util.tensor2img(output.squeeze()),
+        os.path.join('../results', 'fake.png'))
