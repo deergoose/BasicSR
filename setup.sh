479ed4734357ac688f59de0257ab450f9ac69a14
Zhou Fang <zhoufang@ucsd.edu>
2019-02-08 20:56:41 -0800

    debug

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get -y install vim libglib2.0-0 libsm6 libxext6 libxrender-dev unzip
+apt-get update && apt-get -y install vim libglib2.0-0 libsm6 libxext6 libxrender-dev unzip screen
 
 pip install --user opencv-python matplotlib pandas tifffile shapely descartes seaborn lmdb
 
