f98f9a989304fc5790d74a28034b30238714e3e8
Zhou Fang <zhoufang@ucsd.edu>
2019-02-19 23:42:28 -0800

    add new pip dependency

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get -y install vim libglib2.0-0 libsm6 libxext6 libxrender-dev unzip screen
 
-pip install --user opencv-python matplotlib pandas tifffile shapely descartes seaborn lmdb
+pip install --user opencv-python matplotlib pandas tifffile shapely descartes seaborn lmdb tensorboardX
 
 git clone https://github.com/coufon/BasicSR.git
