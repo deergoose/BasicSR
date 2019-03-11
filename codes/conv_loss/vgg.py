86f2fc5e944fcc37759cf364fc86249801c80302
zhoufang@google.com <zhoufang@google.com>
2019-03-10 18:33:53 -0700

    update loss demo

--- codes/conv_loss/vgg.py
+++ codes/conv_loss/vgg.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torchvision
 import os
 
-_RESULT_DIR = '/Users/zhoufang/Desktop/lu/esrgan/results/gan_paper/results_100k'
+_RESULT_DIR = '/Users/zhoufang/Desktop/lu/esrgan/results/gan_paper/results_vgg_6_100k/results_vgg_34'
 
 
 # Assume input range is [0, 1]
@@ -22,9 +22,9 @@ class VGGFeatureExtractor(nn.Module):
             model = torchvision.models.vgg19(pretrained=True)
         self.use_input_norm = use_input_norm
         if self.use_input_norm:
-            mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(device)
+            mean = torch.Tensor([0.485-1, 0.456-1, 0.406-1]).view(1, 3, 1, 1).to(device)
             # [0.485-1, 0.456-1, 0.406-1] if input in range [-1,1]
-            std = torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(device)
+            std = torch.Tensor([0.229*2, 0.224*2, 0.225*2]).view(1, 3, 1, 1).to(device)
             # [0.229*2, 0.224*2, 0.225*2] if input in range [-1,1]
             self.register_buffer('mean', mean)
             self.register_buffer('std', std)
@@ -48,8 +48,8 @@ def define_F(opt, use_bn=False):
         feature_layer = 49
     else:
         feature_layer = 34
-    netF = VGGFeatureExtractor(feature_layer=feature_layer, use_bn=use_bn, \
-        use_input_norm=False, device=device)
+    netF = VGGFeatureExtractor(feature_layer=16, use_bn=use_bn, \
+        use_input_norm=True, device=device)
     # netF = ResNet101FeatureExtractor(use_input_norm=True, device=device)
     if gpu_ids:
         netF = nn.DataParallel(netF)
@@ -79,7 +79,7 @@ def denormalize(im):
 
 def read_im(im_dir, im_name):
     im = cv2.imread(os.path.join(im_dir, im_name))
-    im = im[:128, :128, :]
+    #im = im[:128, :128, :]
     im = normalize(im.astype(np.float) / 255.0 * 2100)
     im = im[np.newaxis, ...]
     return torch.from_numpy(np.ascontiguousarray(
@@ -96,8 +96,8 @@ def main():
 
     netF = define_F(opt, use_bn=False).to(device)
 
-    im_hr = read_im(_RESULT_DIR, '5_hr.png')
-    im_fake = read_im(_RESULT_DIR, '5_fake.png')
+    im_hr = read_im(_RESULT_DIR, '15_hr.png')
+    im_fake = read_im(_RESULT_DIR, '15_fake.png')
 
     real_fea = netF(im_hr)
     fake_fea = netF(im_fake)
@@ -106,8 +106,8 @@ def main():
 
     real_feat = np.transpose(real_fea.numpy()[0], (1, 2, 0))
     fake_feat = np.transpose(fake_fea.numpy()[0], (1, 2, 0))
-    print real_feat[..., 0]
-    print fake_feat[..., 0]
+    feat_diff = np.absolute(real_feat - fake_feat)
+    print 'sum: ', np.sum(feat_diff)
 
     for i in range(real_feat.shape[2]):
         im_real_feat = cv2.normalize(
@@ -115,10 +115,15 @@ def main():
             norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
         im_fake_feat = cv2.normalize(
             fake_feat[..., i], None, 0, 255,
-            norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)  
+            norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        im_feat_diff = cv2.normalize(
+            feat_diff[..., i], None, 0, 255,
+            norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
-        cv2.imshow('real feature', im_real_feat)
-        cv2.imshow('fake feature', im_fake_feat)
+        print np.sum(feat_diff[..., i])
+        cv2.imshow('real', im_real_feat)
+        cv2.imshow('fake', im_fake_feat)
+        cv2.imshow('diff', im_feat_diff)
         cv2.waitKey()
 
 
