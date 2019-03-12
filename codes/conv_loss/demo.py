179d97a99b44052eac319e8842143a9fb0c45c7a
zhoufang@google.com <zhoufang@google.com>
2019-03-11 20:50:24 -0700

    add pnasnet in conv loss

--- codes/conv_loss/demo.py
+++ codes/conv_loss/demo.py
@@ -2,55 +2,37 @@ import cv2
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision
 import os
+import pretrainedmodels
+import pretrainedmodels.utils as utils
+
+from models import VGGFeatureExtractor, PNasNetFeatureExtractor
 
 _RESULT_DIR = '/Users/zhoufang/Desktop/lu/esrgan/results/gan_paper/results_vgg_6_100k/results_vgg_34'
 
-
-# Assume input range is [0, 1]
-class VGGFeatureExtractor(nn.Module):
-    def __init__(self,
-                 feature_layer=34,
-                 use_bn=False,
-                 use_input_norm=True,
-                 device=torch.device('cpu')):
-        super(VGGFeatureExtractor, self).__init__()
-        if use_bn:
-            model = torchvision.models.vgg19_bn(pretrained=True)
-        else:
-            model = torchvision.models.vgg19(pretrained=True)
-        self.use_input_norm = use_input_norm
-        if self.use_input_norm:
-            mean = torch.Tensor([0.485-1, 0.456-1, 0.406-1]).view(1, 3, 1, 1).to(device)
-            # [0.485-1, 0.456-1, 0.406-1] if input in range [-1,1]
-            std = torch.Tensor([0.229*2, 0.224*2, 0.225*2]).view(1, 3, 1, 1).to(device)
-            # [0.229*2, 0.224*2, 0.225*2] if input in range [-1,1]
-            self.register_buffer('mean', mean)
-            self.register_buffer('std', std)
-        self.features = nn.Sequential(*list(model.features.children())[:(feature_layer + 1)])
-        # No need to BP to variable
-        for k, v in self.features.named_parameters():
-            v.requires_grad = False
-
-    def forward(self, x):
-        if self.use_input_norm:
-            x = (x - self.mean) / self.std
-        output = self.features(x)
-        return output
-
+opt = {
+    'gpu_ids': None,
+    'model_name': 'pnasnet'
+}
 
 def define_F(opt, use_bn=False):
     gpu_ids = opt['gpu_ids']
+    model_name = opt['model_name']
     device = torch.device('cuda' if gpu_ids else 'cpu')
-    # pytorch pretrained VGG19-54, before ReLU.
-    if use_bn:
-        feature_layer = 49
+    if model_name == 'vgg':
+        # pytorch pretrained VGG19-54, before ReLU.
+        if use_bn:
+            feature_layer = 49
+        else:
+            feature_layer = 34
+        netF = VGGFeatureExtractor(feature_layer=16, use_bn=use_bn,
+            use_input_norm=True, device=device)
+        # netF = ResNet101FeatureExtractor(use_input_norm=True, device=device)
+    elif model_name == 'pnasnet':
+        netF = PNasNetFeatureExtractor()
     else:
-        feature_layer = 34
-    netF = VGGFeatureExtractor(feature_layer=16, use_bn=use_bn, \
-        use_input_norm=True, device=device)
-    # netF = ResNet101FeatureExtractor(use_input_norm=True, device=device)
+        print('model {} not supported.'.format(model_name))
+        return None
     if gpu_ids:
         netF = nn.DataParallel(netF)
     netF.eval()  # No need to train
@@ -87,17 +69,14 @@ def read_im(im_dir, im_name):
 
 
 def main():
-    opt = {
-        'gpu_ids': None
-    }
     device = torch.device('cuda' if opt['gpu_ids'] is not None else 'cpu')
     cri_fea = nn.L1Loss().to(device)
     #cri_fea = nn.MSELoss().to(device)
 
     netF = define_F(opt, use_bn=False).to(device)
 
-    im_hr = read_im(_RESULT_DIR, '15_hr.png')
-    im_fake = read_im(_RESULT_DIR, '15_fake.png')
+    im_hr = read_im(_RESULT_DIR, '7_hr.png')
+    im_fake = read_im(_RESULT_DIR, '7_fake.png')
 
     real_fea = netF(im_hr)
     fake_fea = netF(im_fake)
@@ -107,6 +86,7 @@ def main():
     real_feat = np.transpose(real_fea.numpy()[0], (1, 2, 0))
     fake_feat = np.transpose(fake_fea.numpy()[0], (1, 2, 0))
     feat_diff = np.absolute(real_feat - fake_feat)
+    print real_feat.shape
     print 'sum: ', np.sum(feat_diff)
 
     for i in range(real_feat.shape[2]):
---
+++ codes/conv_loss/models.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torchvision
+
+import pretrainedmodels.models.pnasnet as pnasnet
+
+
+# Assume input range is [0, 1]
+class VGGFeatureExtractor(nn.Module):
+    def __init__(self,
+                 feature_layer=34,
+                 use_bn=False,
+                 use_input_norm=True,
+                 device=torch.device('cpu')):
+        super(VGGFeatureExtractor, self).__init__()
+        if use_bn:
+            model = torchvision.models.vgg19_bn(pretrained=True)
+        else:
+            model = torchvision.models.vgg19(pretrained=True)
+        self.use_input_norm = use_input_norm
+        if self.use_input_norm:
+            mean = torch.Tensor([0.485-1, 0.456-1, 0.406-1]).view(1, 3, 1, 1).to(device)
+            # [0.485-1, 0.456-1, 0.406-1] if input in range [-1,1]
+            std = torch.Tensor([0.229*2, 0.224*2, 0.225*2]).view(1, 3, 1, 1).to(device)
+            # [0.229*2, 0.224*2, 0.225*2] if input in range [-1,1]
+            self.register_buffer('mean', mean)
+            self.register_buffer('std', std)
+        self.features = nn.Sequential(*list(model.features.children())[:(feature_layer + 1)])
+        # No need to BP to variable
+        for k, v in self.features.named_parameters():
+            v.requires_grad = False
+
+    def forward(self, x):
+        if self.use_input_norm:
+            x = (x - self.mean) / self.std
+        output = self.features(x)
+        return output
+
+
+def pnasnet_features(self, x):
+    x_conv_0 = self.conv_0(x)
+    x_stem_0 = self.cell_stem_0(x_conv_0)
+    x_stem_1 = self.cell_stem_1(x_conv_0, x_stem_0)
+    x_cell_0 = self.cell_0(x_stem_0, x_stem_1)
+    x_cell_1 = self.cell_1(x_stem_1, x_cell_0)
+    x_cell_2 = self.cell_2(x_cell_0, x_cell_1)
+    x_cell_3 = self.cell_3(x_cell_1, x_cell_2)
+    x_cell_4 = self.cell_4(x_cell_2, x_cell_3)
+    x_cell_5 = self.cell_5(x_cell_3, x_cell_4)
+    x_cell_6 = self.cell_6(x_cell_4, x_cell_5)
+    x_cell_7 = self.cell_7(x_cell_5, x_cell_6)
+    x_cell_8 = self.cell_8(x_cell_6, x_cell_7)
+    x_cell_9 = self.cell_9(x_cell_7, x_cell_8)
+    x_cell_10 = self.cell_10(x_cell_8, x_cell_9)
+    x_cell_11 = self.cell_11(x_cell_9, x_cell_10)
+    return x_cell_11
+
+pnasnet.PNASNet5Large.features = pnasnet_features
+
+class PNasNetFeatureExtractor(nn.Module):
+    def __init__(self,
+                 use_input_norm=True,
+                 device=torch.device('cpu')):
+        super(PNasNetFeatureExtractor, self).__init__()
+        self.use_input_norm = use_input_norm
+        self.model = pnasnet.PNASNet5Large(num_classes=1001)
+        self.model.load_state_dict(model_zoo.load_url(
+            'http://data.lip6.fr/cadene/pretrainedmodels/pnasnet5large-bf079911.pth'))
+        if self.use_input_norm:
+            mean = torch.Tensor([0.5-1, 0.5-1, 0.5-1]).view(1, 3, 1, 1).to(device)
+            # [0.5-1, 0.5-1, 0.5-1] if input in range [-1,1]
+            std = torch.Tensor([0.5*2, 0.5*2, 0.5*2]).view(1, 3, 1, 1).to(device)
+            # [0.5*2, 0.5*2, 0.5*2] if input in range [-1,1]
+            self.register_buffer('mean', mean)
+            self.register_buffer('std', std)
+        self.features = self.model.features
+        # No need to BP to variable
+        for k, v in self.model.named_parameters():
+            v.requires_grad = False
+
+    def forward(self, x):
+        if self.use_input_norm:
+            x = (x - self.mean) / self.std
+        output = self.features(x)
+        return output
